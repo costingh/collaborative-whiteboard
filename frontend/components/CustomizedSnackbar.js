4b0f99a2a243056d516e426d1c18003fd4989ba7
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-09-02 21:20:33 +0300

    Improved snackbar component

--- frontend/components/CustomizedSnackbar.js
+++ frontend/components/CustomizedSnackbar.js
@@ -1,26 +1,71 @@
+// react
+import React, { useState, useEffect } from 'react'
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
-function Alert(props) {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
+import Slide from '@material-ui/core/Slide';
+
+function TransitionLeft(props) {
+  return <Slide {...props} direction="left" />;
 }
-export default function CustomizedSnackbar({open, setShowSnackbar, snackbarMsg}) {
+
+function TransitionUp(props) {
+  return <Slide {...props} direction="up" />;
+}
+
+function TransitionRight(props) {
+  return <Slide {...props} direction="right" />;
+}
+
+function TransitionDown(props) {
+  return <Slide {...props} direction="down" />;
+}
+
+const Alert = React.forwardRef((props, ref) => <MuiAlert elevation={6} variant="filled" {...props} ref={ref} />);
+const SnackbarComponent = React.forwardRef((props, ref) => <Snackbar  autoHideDuration={4000}  {...props} ref={ref} />);
+
+export default function CustomizedSnackbar({open, setShowSnackbar, snackbarMsg, severity, transition, verticalAnchor, horizontalAnchor }) {
+  const [vertical, setVertical] = useState('bottom');
+  const [horizontal, setHorizontal] = useState('center');
 
   const handleClose = (event, reason) => {
     if (reason === 'clickaway') {
       return;
     }
-
     setShowSnackbar(false);
   };
 
+  useEffect(() => {
+    if(verticalAnchor) {
+      setVertical(verticalAnchor)
+    }
+
+    if(horizontalAnchor) {
+      setHorizontal(horizontalAnchor)
+    }
+  }, [])
 
   return (
     <div style={{position: 'absolute'}}>
-      <Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success">
-          {snackbarMsg}
-        </Alert>
-      </Snackbar>
-    </div>
+      <SnackbarComponent
+          open={open} 
+          onClose={handleClose}
+          TransitionComponent={
+              transition ==='right' 
+                ? TransitionRight 
+                : transition ==='left'
+                  ? TransitionLeft
+                  : transition === 'down'
+                    ? TransitionDown
+                    : transition === 'up'
+                      ? TransitionUp
+                      : TransitionUp 
+          }
+          anchorOrigin={{ vertical, horizontal }}
+        >
+          <Alert onClose={handleClose} severity={severity ? severity : 'success'}>
+            {snackbarMsg}
+          </Alert>
+      </SnackbarComponent>
+      </div>
   );
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,15 @@
+// react
 import React, { useState, useEffect, useRef } from 'react'
+// components
 import Layout from '../components/layout'
 import Canvas from '../components/Canvas'
+import CustomizedSnackbar from '../components/CustomizedSnackbar'
+// context
 import {ThemeProvider} from "../context/ThemeContext"
+// sockets
 import SockJS from 'sockjs-client'
 import Stomp from 'stompjs';
+// method to generate ramdom strings
 import randomString from 'random-string'
 // random username generator
 import {generateRandomUsername} from '../utils/utils';
@@ -11,25 +17,36 @@ import {generateRandomUsername} from '../utils/utils';
 const SOCKET_URL = 'http://localhost:8080/ws-message';
 
 export default function Home() {  
-  const [userId, setUserId] = useState(randomString({length: 15}));
+  	const [userId, setUserId] = useState(randomString({length: 15}));
 	const [roomId, setRoomId] = useState(randomString({length: 15}));
 	const [incomingDrawings, setIncomingDrawings] = useState(null);
-  const [username, setUsername] = useState(generateRandomUsername())
-    
+  	const [username, setUsername] = useState(generateRandomUsername());
+	const [snackbarOpen, setSnackbarOpen] = useState(false);
+	const [snackbarMsg, setSnackbarMsg] = useState('');
+
 	const ws = useRef(null);
 	const stomp = useRef(null);
 
     useEffect(() => {
         ws.current = new SockJS(SOCKET_URL);
-        ws.current.onopen = () => console.log("ws opened");
-        ws.current.onclose = () => console.log("ws closed");
+        ws.current.onopen = () => alert("ws opened");
+        ws.current.onclose = () => alert("ws closed");
 
 		stomp.current = Stomp.over(ws.current);
 		stomp.current.reconnect_delay = 5000;
         stomp.current.connect({}, frame => {
-          stomp.current.subscribe(`/topic/${roomId}`, coordinates => {
-            setIncomingDrawings(JSON.parse(coordinates.body)) 
-          });
+			const message = 'User ' + username + ' has joined the room!' ;
+			stomp.current.send(`/app/send/${roomId}/user`, {}, JSON.stringify(message));
+
+			stomp.current.subscribe(`/topic/${roomId}/user`, msg => {
+				console.log(msg.body)
+				setSnackbarMsg(JSON.parse(msg.body));
+				setSnackbarOpen(true);
+			});
+
+			stomp.current.subscribe(`/topic/${roomId}`, coordinates => {
+				setIncomingDrawings(JSON.parse(coordinates.body)) 
+			});
         });
 
         return () => {
@@ -46,13 +63,22 @@ export default function Home() {
     <Layout>
       <ThemeProvider>
         <Canvas
-          sendMessage={sendMessage}
-          setRoomId={setRoomId}
-          incomingDrawings={incomingDrawings}
-          roomId={roomId}
-		      userId={userId}
-          setUsername={setUsername}
+			sendMessage={sendMessage}
+			setRoomId={setRoomId}
+			incomingDrawings={incomingDrawings}
+			roomId={roomId}
+			userId={userId}
+          	setUsername={setUsername}
         />
+		<CustomizedSnackbar 
+			open={snackbarOpen} 
+			setShowSnackbar={setSnackbarOpen} 
+			snackbarMsg={snackbarMsg} 
+			severity='info' 
+			transition='left' 
+			verticalAnchor='top' 
+			horizontalAnchor='right'
+		/>
       </ThemeProvider>
     </Layout>
   )
